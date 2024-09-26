ae27fab45d65c07ee1ac8bdef15bb3dcd1f9d1ea
Limber Jhonathan <limberjhonathan@gmail.com>
2024-09-25 23:14:47 -0300

    ocp

---
+++ src/ocp/main.ts
@@ -0,0 +1,25 @@
+import { Messaging } from "./services/messaging"
+import { Order } from "./classes/order"
+import { Persistency } from "./services/persistency"
+import { Product } from "./classes/product"
+import { ShoppingCart } from "./classes/shopping-carts"
+import { FiftyPercentDiscount, NoDiscount, TenPercentDiscount } from "./classes/discount"
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount()
+// const tenPercentDiscount =  new TenPercentDiscount()
+const noDiscount = new NoDiscount()
+const shoppingCart = new ShoppingCart(noDiscount)
+const messaging = new Messaging()
+const persistency = new Persistency()
+const order = new Order (shoppingCart, messaging, persistency)
+
+shoppingCart.addItem(new Product("Camiseta", 49.9))
+shoppingCart.addItem(new Product("Caderno", 9.9))
+shoppingCart.addItem(new Product("Lapiz", 1.59))
+// shoppingCart.clear()
+
+console.log(shoppingCart.items)
+console.log(shoppingCart.total())
+console.log(shoppingCart.totalWithDiscount())
+order.checkout()
+console.log(order.orderStatus)
