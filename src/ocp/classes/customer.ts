83eaa2123ba44dcdea60f766ac3a07301fb73b63
Limber Jhonathan <limberjhonathan@gmail.com>
2024-09-27 23:32:16 -0300

    ajustes

--- src/ocp/classes/customer.ts
+++ src/ocp/classes/customer.ts
@@ -4,9 +4,25 @@ export class IndividualCustomer implements CustomerProtocol {
     firstName: string;
     lastName: string;
     cpf: string;
+    cnpj: string;
 
     constructor(firstName: string, lastName: string, cpf: string){
-        
+        this.firstName = 'firstName';
+        this.lastName = 'latName';
+        this.cpf = cpf;
+        this.cnpj = ''
     }
 }
-export class EnterpriseCustomer implements CustomerProtocol {}
+export class EnterpriseCustomer implements CustomerProtocol {
+    firstName: string;
+    lastName: string;
+    cpf: string;
+    cnpj: string;
+
+    constructor(firstName: string, lastName: string, cpf: string){
+        this.firstName = 'firstName';
+        this.lastName = 'latName';
+        this.cpf = cpf;
+        this.cnpj = ''
+    }
+}
